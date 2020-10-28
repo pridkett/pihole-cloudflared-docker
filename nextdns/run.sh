32f69cff5fbd3b7a685696e6bdf13786b07d3be7
Patrick Wagstrom <patrick@wagstrom.net>
2020-10-28 15:17:24 -0400

    feat(nextdns): add support for nextdns as resolver
    Taking this a step further, it was nice to use Cloudflare and
    `cloudflared` as my DNS resolver thanks to DNS over HTTPS, but t's
    not all that configurable. This change brings in NextDNS support as
    the DNS provider and configured it so it runs in another container, with
    an override of the TTL by creating a new copy of `/var/nextdns/run.sh`.
    Eventually that TTL should be configurable through and environment
    variable and I should submit a patch to the upstream, but this works
    now.
    
    It should automatically fail back to Cloudflare when NextDNS is not
    avialable.
    
    DCO 1.1 Signed-off-by: Patrick Wagstrom <patrick@wagstrom.net>

---
+++ nextdns/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NEXTDNS_CONFIG_ID=$NEXTDNS_CONFIG
+NEXTDNS_ARGUMENTS="-listen :8053 -report-client-info -log-queries -cache-size=10MB -max-ttl=240s"
+
+/etc/init.d/dnsmasq restart
+
+echo "Parsing configuration"
+
+while IFS='=' read -r -d '' n v; do
+    if [[ "$n" = "NEXTDNS_CONFIG_"* ]]; then
+        echo " => Found conditional config: $n => $v"
+        NEXTDNS_ARGUMENTS+=" -config $v"
+    fi
+done < <(env -0)
+
+if [ -n "$NEXTDNS_CONFIG" ]; then
+    NEXTDNS_ARGUMENTS+=" -config $NEXTDNS_CONFIG_ID"
+    echo " => Found base NextDNS Config: $NEXTDNS_CONFIG_ID"
+fi
+
+if [ -n "$NEXTDNS_FORWARDING_DOMAIN" ]; then
+    if [ -n "$NEXTDNS_FORWARDING_DNSIP" ]; then
+        NEXTDNS_ARGUMENTS+="  -forwarder $NEXTDNS_FORWARDING_DOMAIN=$NEXTDNS_FORWARDING_DNSIP"
+    fi
+fi
+
+echo "Running nextdns with arguments: $NEXTDNS_ARGUMENTS"
+
+/usr/bin/nextdns run $NEXTDNS_ARGUMENTS
